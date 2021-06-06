b587342c3a04af07cd91b9a907f4f285f17e91fe
sohan2410 <bandarysohan24@gmail.com>
2021-06-06 20:08:11 +0530

    unmade controllers/auth.js changes

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const client = require("../configs/db");
@@ -7,71 +6,10 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-// exports.signUp = (req, res) => {
-//   console.log("entered in signup mode");
-//   const { fullname, email, phonenumber, password, dob} = req.body;
-//   client
-//     .query(`SELECT * FROM details WHERE email = '${email}';`)
-//     .then((data) => {
-//       isValid = data.rows;
-
-//       if (isValid.length !== 0) {
-//         res.status(400).json({
-//           message: "user already exists",
-//         });
-//       } else {
-//         bcrypt.hash(password, 10, (err, hash) => {
-//           if (err) {
-//             res.status(500).json({
-//               message: "internal server error occured",
-//             });
-//           }
-
-//           const user = {
-//             fullname,
-//             email,
-//             phonenumber,
-//             password: hash,
-//             dob,
-//           };
-
-//           client
-//             .query(
-//               `INSERT INTO details (fullname, email, phonenumber, password, dob) VALUES  ('${user.fullname}','${user.email}' , '${user.phonenumber}','${user.password}','${user.dob}');`
-//             )
-//             .then((data) => {
-//               console.log(data);
-//               const token = jwt.sign(
-//                 {
-//                   email: email,
-//                 },
-//                 process.env.SECRET_KEY
-//               );
-
-//               res.status(200).json({
-//                 message: "user added successfully",
-//                 token: token,
-//               });
-//             })
-//             .catch((err) => {
-//               console.log(err);
-//               res.status(500).json({
-//                 message: "Database error occured!",
-//               });
-//             });
-//         });
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(500).json({
-//         message: "Database error occured!!",
-//       });
-//     });
-// };
-
 exports.signUp = (req, res) => {
   console.log("entered in signup mode");
-  const { fullname, email, password } = req.body;
+  const { name, email, password } = req.body;
+
   client
     .query(`SELECT * FROM details WHERE email = '${email}';`)
     .then((data) => {
@@ -79,28 +17,27 @@ exports.signUp = (req, res) => {
 
       if (isValid.length !== 0) {
         res.status(400).json({
-          message: "user already exists",
+          error: "user already exists",
         });
       } else {
         bcrypt.hash(password, 10, (err, hash) => {
           if (err) {
             res.status(500).json({
-              message: "internal server error occured",
+              error: "internal server error occured",
             });
           }
 
           const user = {
-            fullname,
+            name,
             email,
             password: hash,
           };
 
           client
             .query(
-              `INSERT INTO details (fullname, email, password) VALUES  ('${user.fullname}','${user.email}','${user.password}');`
+              `INSERT INTO details (name, email, password) VALUES ('${user.name}', '${user.email}' , '${user.password}');`
             )
             .then((data) => {
-              console.log(data);
               const token = jwt.sign(
                 {
                   email: email,
@@ -114,9 +51,8 @@ exports.signUp = (req, res) => {
               });
             })
             .catch((err) => {
-              console.log(err);
               res.status(500).json({
-                message: "Database error occured!",
+                error: "Database error occured!",
               });
             });
         });
@@ -124,7 +60,7 @@ exports.signUp = (req, res) => {
     })
     .catch((err) => {
       res.status(500).json({
-        message: "Database error occured!!",
+        error: "Database error occured!",
       });
     });
 };
@@ -138,13 +74,13 @@ exports.signIn = (req, res) => {
       userData = data.rows;
       if (userData.length === 0) {
         res.status(400).json({
-          message: "user does not exist, signup instead!",
+          error: "user does not exist, signup instead!",
         });
       } else {
         bcrypt.compare(password, userData[0].password, (err, result) => {
           if (err) {
             res.status(500).json({
-              message: "server error",
+              error: "server error",
             });
           } else if (result === true) {
             const token = jwt.sign(
@@ -159,7 +95,7 @@ exports.signIn = (req, res) => {
             });
           } else {
             res.status(400).json({
-              message: "Enter correct password!",
+              error: "Enter correct password!",
             });
           }
         });
@@ -167,7 +103,7 @@ exports.signIn = (req, res) => {
     })
     .catch((err) => {
       res.status(500).json({
-        message: "database error occured!",
+        error: "database error occured!",
       });
     });
 };
